b8e273e672e7146dcdccc186d23650f04afd27d3
Yugender025 <pentapuriyugender@gmail.com>
2022-10-13 23:02:56 +0530

    bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser= require("body-parser");
 const ejs =require("ejs");
 const mongoose=require("mongoose");
-const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const app =express();
 
 app.set('view engine','ejs');
@@ -16,7 +17,7 @@ const userSchema=new mongoose.Schema({
   password:String
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
 const User = new mongoose.model("User",userSchema);
 
 
@@ -30,17 +31,20 @@ app.get("/register",function(req,res){
   res.render("register");
 });
 app.post("/register",function(req,res){
-  const newUser= new User({
-    email: req.body.username,
-    password: req.body.password
+  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+    const newUser= new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log("err");
+      }else{
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if(err){
-      console.log("err");
-    }else{
-      res.render("secrets");
-    }
-  });
+
 });
 app.post("/login",function(req,res){
   const username=req.body.username;
@@ -50,15 +54,22 @@ app.post("/login",function(req,res){
       console.log(err);
     }else{
       if(founduser){
-        if(founduser.password===password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, founduser.password, function(err, result) {
+            if(result===true){
+                res.render("secrets");
+            }
+          });
+
+
       }
     }
   });
 });
+app.get("/submit",function(req,res){
+     res.render("home");
+});
 app.post("/submit",function(req,res){
-
+  req.body.secret;
 })
 app.listen(3000,function(){
   console.log("port sucessfully working");
